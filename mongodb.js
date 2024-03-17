0428e955ebc69671288be1f4f4890e22ebf52d18
NIKHIL NAILWAL <78683932+nailwal08@users.noreply.github.com>
2024-03-17 20:39:40 +0530

    initial commit

---
+++ mongodb.js
@@ -0,0 +1,20 @@
+//path: middleware/mongodb.js
+import mongoose from 'mongoose';
+
+const connection = {};
+
+async function dbConnect() {
+  if (connection.isConnected) {
+    return;
+  }
+
+  const db = await mongoose.connect('mongodb://localhost:27017', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  connection.isConnected = db.connections[0].readyState;
+}
+
+export default dbConnect;
+
